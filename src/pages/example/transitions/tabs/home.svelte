16d7423aeb57b5ce1e82d727c70f35a70cd19932
twinstae <rabolution@gmail.com>
2021-02-13 16:51:22 +0900

    template init
    svelte, ts, routify, tailwind

---
+++ src/pages/example/transitions/tabs/home.svelte
@@ -0,0 +1,20 @@
+<style lang="postcss">
+  .red-square {
+    @apply border-2 border-red-600 h-32 w-32;
+  }
+
+  main {
+    background: #fd6e64;
+    height: 100%;
+    overflow: auto;
+  }
+</style>
+
+<!-- routify:options index=0 -->
+<main>
+  <br />
+  <h1>Home</h1>
+  <div class="red-square rounded-lg">
+    welcome home!
+  </div>
+</main>
